00591676a1042a01518c3e4ad07fa877d400f01e
Caleb Spear <cspear@cognex.com>
2023-04-27 23:41:19 -0500

    Lab 20, added services and dependencies injection

--- src/app/projects/projects-container/projects-container.component.ts
+++ src/app/projects/projects-container/projects-container.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MOCK_PROJECTS } from '../shared/mock-projects';
 import { Project } from '../shared/project.model';
+import { ProjectService } from '../shared/project.service';
 
 @Component({
   selector: 'app-projects-container',
@@ -8,10 +9,14 @@ import { Project } from '../shared/project.model';
   styleUrls: ['./projects-container.component.css'],
 })
 export class ProjectsContainerComponent implements OnInit {
-  projects: Project[] = MOCK_PROJECTS;
-  constructor() {}
+  projects: Project[] = []; // MOCK_PROJECTS;
+  constructor(private projectService: ProjectService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.projectService.list().subscribe((data) => {
+      this.projects = data;
+    });
+  }
 
   onSaveListItem(event: any) {
     const project: Project = event.item;
